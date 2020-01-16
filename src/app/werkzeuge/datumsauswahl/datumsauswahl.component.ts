b5db84093dcc92bea0bfc5a99e7286d7db8a65dc
Tsiana <alexroeber@users.noreply.github.com>
2020-01-16 11:18:05 +0100

    Finishing Werkzeug Datumsauswahl

--- src/app/werkzeuge/datumsauswahl/datumsauswahl.component.ts
+++ src/app/werkzeuge/datumsauswahl/datumsauswahl.component.ts
@@ -1,16 +1,28 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, EventEmitter, Output} from "@angular/core";
+import {Datum} from "../../fachwerte/Datum";
 
 @Component({
   selector: "datumsauswahl",
   templateUrl: "./datumsauswahl.component.html",
   styleUrls: ["./datumsauswahl.component.scss"]
 })
-export class DatumsauswahlComponent implements OnInit {
+export class DatumsauswahlComponent {
+  @Output() dateChanged: EventEmitter<Datum>;
+
+  ausgewaehltesDatum: Datum;
 
   constructor() {
+    this.dateChanged = new EventEmitter<Datum>();
+    this.ausgewaehltesDatum = new Datum(2, 2, 2020);
   }
 
-  ngOnInit() {
+  zurueck() {
+    this.ausgewaehltesDatum = this.ausgewaehltesDatum.vorherigerTag();
+    this.dateChanged.emit(this.ausgewaehltesDatum);
   }
 
+  weiter() {
+    this.ausgewaehltesDatum = this.ausgewaehltesDatum.naechsterTag();
+    this.dateChanged.emit(this.ausgewaehltesDatum);
+  }
 }
